b69645022e8e4a10a5b3503e14d4ccef9df10f49
Naoki Kinoshita <uttnaoki@gmail.com>
2021-08-09 23:31:44 +0900

    seedでquestionを生成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Question.create(name: 'test name 1', title: 'test title 1', content: 'test content 1')
+Question.create(name: 'test name 2', title: 'test title 2', content: 'test content 2')
+Question.create(name: 'test name 3', title: 'test title 3', content: 'test content 3')
