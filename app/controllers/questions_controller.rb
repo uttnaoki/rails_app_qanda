ae79b9cb91d4879f60b3c142e3fe14f9b9f998c2
Naoki Kinoshita <uttnaoki@gmail.com>
2021-08-10 22:39:33 +0900

    質問新規作成機能を実装

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,24 @@
 class QuestionsController < ApplicationController
   def index
-    @questions = Question.all()
+    @questions = Question.all
   end
   
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      redirect_to root_path, notice: 'Success!'
+    else
+      flash[:alert] = 'Failed!'
+      render :new
+    end
+  end
+  
+  def new
+    @question = Question.new
+  end
+  
+  private
+    def question_params
+      params.require(:question).permit(:name, :title, :content)
+    end
 end
