bf65f413bfcb079126c57be1c972ff846cd99342
Naoki Kinoshita <uttnaoki@gmail.com>
2021-08-11 22:15:45 +0900

    回答投稿機能を実装

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,2 +1,16 @@
 class AnswersController < ApplicationController
+  def create
+    @question = Question.find(params[:question_id])
+    @answer = Answer.new(answer_params)
+    if @answer.save
+      redirect_to question_path(@question), notice: 'Success!'
+    else
+      redirect_to question_path(@question), alert: 'Invalid!'
+    end
+  end
+  
+  private
+    def answer_params
+      params.require(:answer).permit(:question_id, :name, :content)
+    end
 end
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -23,6 +23,7 @@ class QuestionsController < ApplicationController
   end
 
   def show
+    @answer = Answer.new
   end
   
   def update
