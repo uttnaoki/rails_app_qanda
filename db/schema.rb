c15c849d7f22f38620f5d376c481319e6b369045
Naoki Kinoshita <uttnaoki@gmail.com>
2021-08-11 19:17:42 +0900

    answerモデルを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_09_140256) do
+ActiveRecord::Schema.define(version: 2021_08_11_101722) do
+
+  create_table "answers", force: :cascade do |t|
+    t.integer "question_id"
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["question_id"], name: "index_answers_on_question_id"
+  end
 
   create_table "questions", force: :cascade do |t|
     t.string "name"
